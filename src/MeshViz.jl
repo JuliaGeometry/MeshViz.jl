4b20b43c5292191cf380457441d74a3a9e1a20b4
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-03-31 19:16:13 -0300

    Initial commit

--- src/MeshViz.jl
+++ src/MeshViz.jl
@@ -1,5 +1,11 @@
 module MeshViz
 
-# Write your package code here.
+using Meshes
+using AbstractPlotting
+
+import AbstractPlotting: convert_arguments
+
+convert_arguments(P::AbstractPlotting.PointBased, pset::PointSet) =
+  convert_arguments(P, [coordinates(pset[i]) for i in 1:nelements(pset)])
 
 end
