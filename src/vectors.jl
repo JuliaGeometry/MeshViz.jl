2fd725f0a770ea6ad8da060cd38a9fb2cedf0480
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-05-31 15:04:10 -0300

    Add recipe for single geometry

--- src/vectors.jl
+++ src/vectors.jl
@@ -27,3 +27,28 @@ function Makie.plot!(plot::Viz{<:Tuple{AbstractVector{<:PointOrGeometry}}})
     decimation   = decimation,
   )
 end
+
+Makie.plottype(::PointOrGeometry) = Viz{<:Tuple{PointOrGeometry}}
+
+function Makie.plot!(plot::Viz{<:Tuple{PointOrGeometry}})
+  # retrieve item object
+  item = plot[:object][]
+
+  # Meshes.jl attributes
+  elementcolor = plot[:elementcolor][]
+  vertexcolor  = plot[:vertexcolor][]
+  facetcolor   = plot[:facetcolor][]
+  showvertices = plot[:showvertices][]
+  showfacets   = plot[:showfacets][]
+  decimation   = plot[:decimation][]
+
+  # fallback to vector recipe
+  viz!(plot, [item],
+    elementcolor = elementcolor,
+    vertexcolor  = vertexcolor,
+    facetcolor   = facetcolor,
+    showvertices = showvertices,
+    showfacets   = showfacets,
+    decimation   = decimation,
+  )
+end
