dccfd7655b46a925d70d81ff39197d76003fa481
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-10-13 16:30:03 -0300

    Add test skeleton

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,28 @@
 using MeshViz
+using Meshes
+using GeoTables
+using ReferenceTests
+using ImageIO
 using Test
 
+import CairoMakie as Mke
+
 @testset "MeshViz.jl" begin
-    # Write your tests here.
+  # 1D Cartesian grid
+  d = CartesianGrid(10)
+  # TODO
+
+  # 2D Cartesian grid
+  d = CartesianGrid(10,10)
+  viz(d)
+  viz(d, showfacets = true)
+  viz(d, showfacets = true, facetcolor = :red)
+  viz(d, elementcolor = 1:100)
+
+  # 3D Cartesian grid
+  d = CartesianGrid(10,10,10)
+  viz(d)
+  viz(d, showfacets = true)
+  viz(d, showfacets = true, facetcolor = :red)
+  viz(d, elementcolor = 1:1000)
 end
