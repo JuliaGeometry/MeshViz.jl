6e37c0938669c4b12704e12aaa0a7e66adec344f
Júlio Hoffimann <julio.hoffimann@gmail.com>
2022-02-06 11:23:07 -0300

    Add visual tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,28 +3,30 @@ using Meshes
 using GeoTables
 using ReferenceTests
 using ImageIO
+using Random
 using Test
 
 import CairoMakie as Mke
 
 @testset "MeshViz.jl" begin
   # 1D point set
-  p = PointSet(rand(Point1, 100))
+  p = PointSet(Point1[(0.,), (1.,), (2.,)])
   # TODO
 
   # 2D point set
-  p = PointSet(rand(Point2, 100))
-  viz(p)
-  viz(p, color = :red)
-  viz(p, color = 1:100)
-  viz(p, color = 1:100, colormap = :inferno)
+  p = PointSet(Point2[(0.,0.), (1.,0.), (1.,1.), (0.,1.)])
+  @test_reference "data/pset2D-1.png" viz(p)
+  @test_reference "data/pset2D-2.png" viz(p, color = :red)
+  @test_reference "data/pset2D-3.png" viz(p, color = 1:4)
+  @test_reference "data/pset2D-4.png" viz(p, color = 1:4, colormap = :inferno)
 
   # 3D point set
-  p = PointSet(rand(Point3, 100))
-  viz(p)
-  viz(p, color = :red)
-  viz(p, color = 1:100)
-  viz(p, color = 1:100, colormap = :inferno)
+  p = PointSet(Point3[(0.,0.,0.), (1.,0.,0.), (1.,1.,0.), (0.,1.,0.),
+                      (0.,0.,1.), (1.,0.,1.), (1.,1.,1.), (0.,1.,1.)])
+  @test_reference "data/pset3D-1.png" viz(p)
+  @test_reference "data/pset3D-2.png" viz(p, color = :red)
+  @test_reference "data/pset3D-3.png" viz(p, color = 1:8)
+  @test_reference "data/pset3D-4.png" viz(p, color = 1:8, colormap = :inferno)
 
   # 1D Cartesian grid
   d = CartesianGrid(10)
@@ -32,118 +34,122 @@ import CairoMakie as Mke
 
   # 2D Cartesian grid
   d = CartesianGrid(10,10)
-  viz(d)
-  viz(d, showfacets = true)
-  viz(d, showfacets = true, facetcolor = :red)
-  viz(d, color = 1:100)
-  viz(d, color = 1:100, colormap = :inferno)
+  @test_reference "data/grid2D-1.png" viz(d)
+  @test_reference "data/grid2D-2.png" viz(d, showfacets = true)
+  @test_reference "data/grid2D-3.png" viz(d, showfacets = true, facetcolor = :red)
+  @test_reference "data/grid2D-4.png" viz(d, color = 1:100)
+  @test_reference "data/grid2D-5.png" viz(d, color = 1:100, colormap = :inferno)
 
   # 3D Cartesian grid
   d = CartesianGrid(10,10,10)
-  viz(d)
-  viz(d, showfacets = true)
-  viz(d, showfacets = true, facetcolor = :red)
-  viz(d, color = 1:1000)
-  viz(d, color = 1:1000, colormap = :inferno)
+  @test_reference "data/grid3D-1.png" viz(d)
+  @test_reference "data/grid3D-2.png" viz(d, showfacets = true)
+  @test_reference "data/grid3D-3.png" viz(d, showfacets = true, facetcolor = :red)
+  @test_reference "data/grid3D-4.png" viz(d, color = 1:1000)
+  @test_reference "data/grid3D-5.png" viz(d, color = 1:1000, colormap = :inferno)
 
   # 2D N-gons
   t = Triangle((1.,0.), (2.,0.), (2.,1.))
+  @test_reference "data/tri2D-1.png" viz(t)      
+  @test_reference "data/tri2D-2.png" viz(t, showboundary = false)
+  @test_reference "data/tri2D-3.png" viz(t, color = :orange)
+  @test_reference "data/tri2D-4.png" viz(t, color = :cyan, boundarycolor = :red)
   q = Quadrangle((0.,0.), (1.,0.), (1.,1.), (0.,1.))
-  viz(t)      
-  viz(t, showboundary = false)
-  viz(t, color = :orange)
-  viz(t, color = :cyan, boundarycolor = :red)
-  viz(q)      
-  viz(q, showboundary = false)
-  viz(q, color = :orange)
-  viz(q, color = :cyan, boundarycolor = :red)
+  @test_reference "data/quad2D-1.png" viz(q)      
+  @test_reference "data/quad2D-2.png" viz(q, showboundary = false)
+  @test_reference "data/quad2D-3.png" viz(q, color = :orange)
+  @test_reference "data/quad2D-4.png" viz(q, color = :cyan, boundarycolor = :red)
 
   # 3D N-gons
   o = Octagon([(0.0,0.0,1.0), (0.5,-0.5,0.0), (1.0,0.0,0.0), (1.5,0.5,-0.5),
                (1.0,1.0,0.0), (0.5,1.5,0.0), (0.0,1.0,0.0), (-0.5,0.5,0.0)])
-  viz(o)
-  viz(o, showboundary = false)
-  viz(o, color = :orange)
-  viz(o, color = :cyan, boundarycolor = :red)
+  @test_reference "data/oct3D-1.png" viz(o)
+  @test_reference "data/oct3D-2.png" viz(o, showboundary = false)
+  @test_reference "data/oct3D-3.png" viz(o, color = :orange)
+  @test_reference "data/oct3D-4.png" viz(o, color = :cyan, boundarycolor = :red)
 
   # Polygonal areas
+  Random.seed!(2020)
   p = PolyArea((0.,0.), (0.5,-1.5), (1.,0.), (1.5,0.5),
                (1.,1.), (0.5,1.5), (-0.5,0.5), (0.,0.))
-  viz(p)      
-  viz(p, showboundary = false)
-  viz(p, color = :orange)
-  viz(p, color = :cyan, boundarycolor = :red)
+  @test_reference "data/poly2D-1.png" viz(p)      
+  @test_reference "data/poly2D-2.png" viz(p, showboundary = false)
+  @test_reference "data/poly2D-3.png" viz(p, color = :orange)
+  @test_reference "data/poly2D-4.png" viz(p, color = :cyan, boundarycolor = :red)
 
   # Multi-geometries
   t = Triangle((1.,0.), (2.,0.), (2.,1.))
   q = Quadrangle((0.,0.), (1.,0.), (1.,1.), (0.,1.))
   m = Multi([t, q])
-  viz(m)      
-  viz(m, showboundary = false)
-  viz(m, color = :orange)
-  viz(m, color = :cyan, boundarycolor = :red)
+  @test_reference "data/multi2D-1.png" viz(m)      
+  @test_reference "data/multi2D-2.png" viz(m, showboundary = false)
+  @test_reference "data/multi2D-3.png" viz(m, color = :orange)
+  @test_reference "data/multi2D-4.png" viz(m, color = :cyan, boundarycolor = :red)
 
   # 2D boxes
   b = Box((0.,0.), (1.,1.))
-  viz(b)      
-  viz(b, showboundary = false)
-  viz(b, color = :orange)
-  viz(b, color = :cyan, boundarycolor = :red)
+  @test_reference "data/box2D-1.png" viz(b)      
+  @test_reference "data/box2D-2.png" viz(b, showboundary = false)
+  @test_reference "data/box2D-3.png" viz(b, color = :orange)
+  @test_reference "data/box2D-4.png" viz(b, color = :cyan, boundarycolor = :red)
 
   # 3D boxes
   b = Box((0.,0.,0.), (1.,1.,1.))
-  viz(b)      
-  viz(b, color = :orange)
+  @test_reference "data/box3D-1.png" viz(b)      
+  @test_reference "data/box3D-2.png" viz(b, color = :orange)
 
   # 3D spheres
   s = Sphere((0.,0.,0.), 1.)
-  viz(s)
-  viz(s, color = :orange)
+  @test_reference "data/sphere3D-1.png" viz(s)
+  @test_reference "data/sphere3D-2.png" viz(s, color = :orange)
 
   # Collections of geometries
   t = Triangle((1.,0.), (2.,0.), (2.,1.))
   q = Quadrangle((0.,0.), (1.,0.), (1.,1.), (0.,1.))
   d = Collection([t, q])
-  viz(d)
-  viz(d, showfacets = true)
-  viz(d, showfacets = true, facetcolor = :red)
-  viz(d, color = 1:2)
-  viz(d, color = 1:2, colormap = :inferno)
+  @test_reference "data/collec2D-1.png" viz(d)
+  @test_reference "data/collec2D-2.png" viz(d, showfacets = true)
+  @test_reference "data/collec2D-3.png" viz(d, showfacets = true, facetcolor = :red)
+  @test_reference "data/collec2D-4.png" viz(d, color = 1:2)
+  @test_reference "data/collec2D-5.png" viz(d, color = 1:2, colormap = :inferno)
 
   # Simple meshes
   s = Sphere((0.,0.,0.), 1.)
   m = discretize(s, RegularDiscretization(10))
-  viz(m)
-  viz(m, color = 1:nvertices(m))
-  viz(m, color = 1:nelements(m))
-  viz(m, color = 1:nelements(m), showfacets = true)
-  viz(m, color = :orange, showfacets = true, facetcolor = :cyan)
+  @test_reference "data/mesh3D-1.png" viz(m)
+  @test_reference "data/mesh3D-2.png" viz(m, color = 1:nvertices(m))
+  @test_reference "data/mesh3D-3.png" viz(m, color = 1:nelements(m))
+  @test_reference "data/mesh3D-4.png" viz(m, color = 1:nelements(m), showfacets = true)
+  @test_reference "data/mesh3D-5.png" viz(m, color = :orange, showfacets = true, facetcolor = :cyan)
 
   # 2D partitions
+  Random.seed!(2020)
   g = CartesianGrid(10, 10)
   p = partition(g, PlanePartition((1.,1.)))
-  viz(p)
+  @test_reference "data/part2D-1.png" viz(p)
 
   # 3D partitions
+  Random.seed!(2020)
   g = CartesianGrid(10, 10, 10)
   p = partition(g, PlanePartition((1.,1.,1.)))
-  viz(p)
+  @test_reference "data/part3D-1.png" viz(p)
 
   # Vector of points
-  p = rand(Point2, 100)
-  viz(p)
-  viz(p, color = 1:100)
-  viz(p, color = 1:100, colormap = :inferno)
-  viz(p, size = 20)
+  p = Point2[(0.,0.), (1.,0.), (1.,1.), (0.,1.)]
+  @test_reference "data/points2D-1.png" viz(p)
+  @test_reference "data/points2D-2.png" viz(p, color = 1:4)
+  @test_reference "data/points2D-3.png" viz(p, color = 1:4, colormap = :inferno)
+  @test_reference "data/points2D-4.png" viz(p, size = 20)
 
   # Data over grid
-  d = meshdata(CartesianGrid(10,10), etable = (z=rand(100),w=1:100))
-  viz(d, variable = :z)
-  viz(d, variable = :w)
-  viz(d, colormap = :inferno)
+  d = meshdata(CartesianGrid(10,10), etable = (z=1:100,w=1:100))
+  @test_reference "data/griddata2D-1.png" viz(d, variable = :z)
+  @test_reference "data/griddata2D-2.png" viz(d, variable = :w)
+  @test_reference "data/griddata2D-3.png" viz(d, colormap = :inferno)
 
   # Data over point set
-  d = meshdata(PointSet(rand(Point2,100)), etable = (z=rand(100),))
-  viz(d)
-  viz(d, colormap = :inferno)
+  p = PointSet(centroid.(CartesianGrid(10,10)))
+  d = meshdata(p, etable = (z=1:100,))
+  @test_reference "data/psetdata2D-1.png" viz(d)
+  @test_reference "data/psetdata2D-2.png" viz(d, colormap = :inferno)
 end
