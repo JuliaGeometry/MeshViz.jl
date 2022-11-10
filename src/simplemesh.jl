fb03a19c40aac45b8a1a293703a2b6c5132dafed
dmillard <dmillard10@gmail.com>
2022-11-10 15:10:30 -0800

    WIP Observables

--- src/simplemesh.jl
+++ src/simplemesh.jl
@@ -6,8 +6,8 @@ Makie.plottype(::SimpleMesh) = Viz{<:Tuple{SimpleMesh}}
 
 function Makie.plot!(plot::Viz{<:Tuple{SimpleMesh}})
   # retrieve mesh object
-  mesh = plot[:object][]
-  rank = paramdim(mesh)
+  mesh = plot[:object]
+  rank = paramdim(mesh[])
 
   # different recipes for meshes with
   # 1D, 2D, 3D, ... ND simplices
@@ -59,79 +59,80 @@ function viz2D!(plot, mesh)
   colorant = process(color, colorscheme, alpha)
 
   # relevant settings
-  dim   = embeddim(mesh)
-  nvert = nvertices(mesh)
-  nelem = nelements(mesh)
-  verts = vertices(mesh)
-  topo  = topology(mesh)
-  elems = elements(topo)
+  makie_mesh = map(mesh) do mesh
+    dim   = embeddim(mesh)
+    nvert = nvertices(mesh)
+    nelem = nelements(mesh)
+    verts = vertices(mesh)
+    topo  = topology(mesh)
+    elems = elements(topo)
 
-  # coordinates of vertices
-  coords = coordinates.(verts)
+    # coordinates of vertices
+    coords = coordinates.(verts)
 
-  # fan triangulation (assume convexity)
-  tris4elem = map(elems) do elem
-    I = indices(elem)
-    [[I[1], I[i], I[i+1]] for i in 2:length(I)-1]
-  end
-
-  # flatten vector of triangles
-  tris = [tri for tris in tris4elem for tri in tris]
-
-  # element vs. vertex coloring
-  if colorant isa AbstractVector
-    ncolor = length(colorant)
-    if ncolor == nelem # element coloring
-      # duplicate vertices and adjust
-      # connectivities to avoid linear
-      # interpolation of colors
-      nt = 0
-      elem4tri = Dict{Int,Int}()
-      for e in 1:nelem
-        Δs = tris4elem[e]
-        for _ in 1:length(Δs)
-          nt += 1
-          elem4tri[nt] = e
-        end
-      end
-      nv = 3nt
-      tcoords = [coords[i] for tri in tris for i in tri]
-      tconnec = [collect(I) for I in Iterators.partition(1:nv, 3)]
-      tcolors = map(1:nv) do i
-        t = ceil(Int, i/3)
-        e = elem4tri[t]
-        colorant[e]
-      end
-    elseif ncolor == nvert # vertex coloring
-      # nothing needs to be done because
-      # this is the default in Makie and
-      # because the triangulation above
-      # does not change the vertices in
-      # the original polygonal mesh
-      tcoords = coords
-      tconnec = tris
-      tcolors = colorant
-    else
-      throw(ArgumentError("Provided $ncolor colors but the mesh has
-                           $nvert vertices and $nelem elements"))
+    # fan triangulation (assume convexity)
+    tris4elem = map(elems) do elem
+      I = indices(elem)
+      [[I[1], I[i], I[i+1]] for i in 2:length(I)-1]
     end
-  else # single color
-    # nothing needs to be done
-    tcoords  = coords
-    tconnec  = tris
-    tcolors  = colorant
+
+    # flatten vector of triangles
+    tris = [tri for tris in tris4elem for tri in tris]
+
+    # element vs. vertex coloring
+    if colorant isa AbstractVector
+      ncolor = length(colorant)
+      if ncolor == nelem # element coloring
+        # duplicate vertices and adjust
+        # connectivities to avoid linear
+        # interpolation of colors
+        nt = 0
+        elem4tri = Dict{Int,Int}()
+        for e in 1:nelem
+          Δs = tris4elem[e]
+          for _ in 1:length(Δs)
+            nt += 1
+            elem4tri[nt] = e
+          end
+        end
+        nv = 3nt
+        tcoords = [coords[i] for tri in tris for i in tri]
+        tconnec = [collect(I) for I in Iterators.partition(1:nv, 3)]
+        tcolors = map(1:nv) do i
+          t = ceil(Int, i/3)
+          e = elem4tri[t]
+          colorant[e]
+        end
+      elseif ncolor == nvert # vertex coloring
+        # nothing needs to be done because
+        # this is the default in Makie and
+        # because the triangulation above
+        # does not change the vertices in
+        # the original polygonal mesh
+        tcoords = coords
+        tconnec = tris
+        tcolors = colorant
+      else
+        throw(ArgumentError("Provided $ncolor colors but the mesh has
+                             $nvert vertices and $nelem elements"))
+      end
+    else # single color
+      # nothing needs to be done
+      tcoords  = coords
+      tconnec  = tris
+      tcolors  = colorant
+    end
+
+    # convert connectivities to matrix format
+    tmatrix = reduce(hcat, tconnec) |> transpose
+
+    # enable shading in 3D
+    shading = dim == 3
+
+    return Makie.convert_arguments(Makie.Mesh, tcoords, tmatrix)[1]
   end
 
-  # convert connectivities to matrix format
-  tmatrix = reduce(hcat, tconnec) |> transpose
-
-  # enable shading in 3D
-  shading = dim == 3
-
-  Makie.mesh!(plot, tcoords, tmatrix,
-    color = tcolors,
-    shading = shading, 
-  )
+  Makie.mesh!(plot, makie_mesh, color=:brown, shading=true)
 
   if showfacets
     # use a sophisticated data structure
