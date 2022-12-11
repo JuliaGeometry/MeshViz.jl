c1b17b0888fdb7d10b976ade7d01feecaadfbe8b
Júlio Hoffimann <julio.hoffimann@gmail.com>
2022-12-11 12:05:38 -0300

    Refactor data.jl

--- src/data.jl
+++ src/data.jl
@@ -2,7 +2,7 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
-function viewer(data::Data; kwargs...)
+function viewer(data::Data)
   # retrieve domain and element table
   dom, tab = domain(data), values(data)
 
@@ -33,20 +33,18 @@ function viewer(data::Data; kwargs...)
   menu   = Makie.Menu(fig[1,2], options = collect(plottable))
 
   # select plottable variable
-  var    = menu.selection
-  vals   = Makie.@lift Tables.getcolumn(cols, $var)
-  scheme = Makie.@lift defaultscheme($vals)
-  color  = Makie.@lift process($vals, $scheme, 1.0)
+  var  = menu.selection
+  vals = Makie.@lift Tables.getcolumn(cols, $var)
+  cmap = Makie.@lift defaultscheme($vals)
 
   # initialize visualization
-  viz(fig[2,:], dom; color = color, colorscheme = scheme, kwargs...)
-  Makie.Colorbar(fig[2,3], colormap = scheme)
+  viz(fig[2,:], dom; color = vals)
+  Makie.Colorbar(fig[2,3], colormap = cmap)
 
   # update visualization if necessary
   Makie.on(menu.selection) do var
-    vals[]   = Tables.getcolumn(cols, var)
-    scheme[] = defaultscheme(vals[])
-    color[]  = process(vals[], scheme[], 1.0)
+    vals[] = Tables.getcolumn(cols, var)
+    cmap[] = defaultscheme(vals[])
   end
 
   fig
